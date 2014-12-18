3b204490fbd75eee7a0df9f4313f3b796170109b
Tim Röhrig <troehrig@stud.hs-bremen.de>
2014-12-18 16:16:04 +0100

    Zugermittlung liefert eine Liste!

--- lki.pl
+++ lki.pl
@@ -7,28 +7,42 @@
 :- dynamic
 	sprungmöglichkeit/2.
 
-
-
-züge(Farbe) :-
+züge(Farbe,_) :-
 	retractall(sprungmöglichkeit(_,_)),
 	write('Mögliche Züge werden berechnet... '),
 	nl,
-	sprünge(Farbe),fail.
+	sprünge(Farbe),
+	fail.
 
-züge(Farbe) :-
+züge(Farbe,_) :-
 	retractall(zugmöglichkeit(_,_)),
-	züg(Farbe),fail.
+	züg(Farbe),
+	fail.
 
-züge(_) :-
-	write('Berechnung abgeschlossen'),nl,
+züge(_,_) :-
+	write('Berechnung abgeschlossen'),
+	nl,
 	(
 	sprungmöglichkeit(S,Z),
-	write(S),write(Z)
+	write(S),write(Z),nl
         );
+	(
 	zugmöglichkeit(S2,Z2),
-	write(S2),write(Z2),nl,fail.
+	write(S2),write(Z2),nl
+	),
+	fail.
 
-züge(_) :- true.
+züge(_,Liste) :-
+	listeZüge([],Liste),!.
+
+listeZüge(ListeVorhanden, ListeErgebnis) :-
+	zugmöglichkeit(S,Z),
+	atom_concat(S,Z,Zug),
+	\+member(Zug,ListeVorhanden),
+	append(ListeVorhanden, [Zug], ListeNeu),
+	listeZüge(ListeNeu,ListeErgebnis).
+
+listeZüge(ListeVorhanden,ListeVorhanden).
 
 sprünge(Head) :-
 	brett(LFeld,[]),
