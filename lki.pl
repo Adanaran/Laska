95ead9e2c68dcda193c86c5b27f452986d15ae2f
Tim Röhrig <troehrig@stud.hs-bremen.de>
2014-12-18 13:43:44 +0100

    Laska KI-Datei mit erster Zugermittlung

---
+++ lki.pl
@@ -0,0 +1,36 @@
+:-[laskazug].
+
+züge(Farbe) :-
+	write('Mögliche Züge: '),
+	nl,
+	sprünge(Farbe).
+züge(Farbe) :-
+	züg(Farbe).
+
+
+sprünge(Head) :-
+	brett(LFeld,[]),
+	brett(FFeld,[Head|_]),
+	opponent(Head,Opp),
+	brett(OFeld,[Opp|_]),
+	nachbarn(LFeld,OFeld,FFeld),
+	write(FFeld),
+	write('-->'),
+	write(LFeld),
+	write(' über '),
+	write(OFeld).
+
+züg(Head) :-
+	brett(LFeld,[]),
+	brett(FFeld,[Head|_]),
+	nachbarn(LFeld,FFeld,_),
+	write(FFeld),
+	write('-->'),
+	write(LFeld).
+
+
+zieh :-
+	ziehen(schwarz,e3d4),
+	ziehen(weiss,c5e3),
+	schreibeBrett(schwarz),!.
+
