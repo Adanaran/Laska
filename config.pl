fe3c006e8a2304cf3a11674ad261f3135805a7e3
Malte Ahlering <mahlering@stud.hs-bremen.de>
2015-01-15 16:05:37 +0100

    Minimax-KI lauffähig

--- config.pl
+++ config.pl
@@ -4,7 +4,7 @@ config(wert_rand,1).
 config(wert_doppelTurm,1).
 config(wert_zug,1).
 config(wert_befreibarerTürme,1).
-config(max_tiefe_ki,3).
+config(max_tiefe_ki,2).
 
 config(wert_SprungLänge_x,1,2):-!.
 config(wert_SprungLänge_x,2,5):-!.
--- minimax.pl
+++ minimax.pl
@@ -1,58 +1,44 @@
-:- dynamic
-	count/1.
+min_am_zug([weiss|_]).
 
-count(0).
+max_am_zug([schwarz|_]).
 
-min_am_zug(weiss).
-
-max_am_zug(schwarz).
+minimax(P,P,V,2):-
+	siegWertung(P,V).
 
 % P = (aktuelle) Position mit Minimax-Wert V,
 % B = Position nach bestem Zug
-minimax(P,B,V) :-
+minimax(P,B,V,T) :-
 % L = Liste der unmittelbaren Nachfolger von P
 %     (aus legalen Zuegen)
 
-	count(C),
-	config(max_tiefe_ki,M),
-	C < M,
-	retractall(count(_)),
-	C1 is C + 1,
-	assert(count(C1)),
+	listeVBretter(P,L),!,
 
-	listeVBretter(P,L),
 	nl,nl,nl,
 	write('L ist'),nl,writeListOfList(L),
 	nl,nl,nl,
-	!,
 
 % Besten Nachfolger ermitteln
-	best(L,B,V),
+	best(L,B,V,T),
 
 	nth0(26,B,Z),
 	nl,nl,nl,
 	write('V:'),write(V),nl,
 	write('B:'),write(B),nl,
 	write('Zug:'),write(Z),nl,
-	nl,nl,nl;
+	nl,nl,nl. %;
 
 % P ist Blattknoten mit statischer Bewertung V
-	siegWertung(P,V),
+%	siegWertung(P,V).
 
-	nth0(26,B,Z),
-	nl,nl,nl,
-	write('V:'),write(V),nl,
-	write('B:'),write(B),nl,
-	write('Zug:'),write(Z),nl,
-	nl,nl,nl.
-
-best([P],P,V) :-
-	minimax(P,_,V), !.
+best([P],P,V,T) :-
+	T1 is T + 1,
+	minimax(P,_,V,T1), !.
 
 % Pm ist von P1 und P2 die bessere Position mit Vm
-best([P1|L],Pm,Vm) :-
-	minimax(P1,_,V1),
-	best(L,P2,V2),
+best([P1|L],Pm,Vm,T) :-
+	T1 is T + 1,
+	minimax(P1,_,V1,T1),
+	best(L,P2,V2,T),
 	besser(P1,V1,P2,V2,Pm,Vm).
 
 % P0 ist besser als P1
