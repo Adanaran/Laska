de432617035d7de1373184a463aad952028ef0c5
Tim Röhrig <troehrig@stud.hs-bremen.de>
2015-01-28 18:29:42 +0100

    config aufgeräumt, spielt jetzt alphabeta gegen alphabeta

--- config.pl
+++ config.pl
@@ -1,47 +1,66 @@
-%% Config-Block für weiss %%
+% Werte schwarz %%
+config(wert_bauer,schwarz,3).
+config(wert_offizier,schwarz,10).
+config(wert_rand,schwarz,5).
+config(wert_doppelTurm,schwarz,10).
+config(wert_zug,schwarz,5).
+config(wert_befreibarerTürme,schwarz,1).
+config(wert_sieg,schwarz,10000).
+
+% Werte weiss %%
+config(wert_bauer,weiss,1).
+config(wert_offizier,weiss,1).
+config(wert_rand,weiss,1).
+config(wert_doppelTurm,weiss,1).
+config(wert_zug,weiss,1).
+config(wert_befreibarerTürme,weiss,1).
+config(wert_sieg,weiss,10000).
+
+
+config(max_tiefe_ki,6).
+config(max_tiefe_ab_ki,10).
+
+
+%Config-Block für weiss %%
 
 config(wert_bauer,BWert):-
 	fehler(nein,schwarz),
-%	config(wert_bauer,weiss,BWert),!.
-	BWert is 3,!.
+	config(wert_bauer,weiss,BWert),!.
 config(wert_offizier,OWert):-
 	fehler(nein,schwarz),
-	OWert is 3,!.
+	config(wert_offizier,weiss,OWert),!.
 config(wert_rand,RWert):-
 	fehler(nein,schwarz),
-	RWert is 1,!.
+	config(wert_rand,weiss,RWert),!.
 config(wert_doppelTurm,DTWert):-
 	fehler(nein,schwarz),
-	DTWert is 1,!.
+	config(wert_doppelTurm,weiss,DTWert),!.
 config(wert_zug,ZWert):-
 	fehler(nein,schwarz),
-	ZWert is 1,!.
+	config(wert_zug,weiss,ZWert),!.
 config(wert_befreibarerTürme,BTWert):-
 	fehler(nein,schwarz),
-	BTWert is 1,!.
+	config(wert_befreibarerTürme,weiss,BTWert),!.
 config(wert_sieg,WSieg):-
 	fehler(nein,schwarz),
-	WSieg is 10000,!.
+	config(wert_sieg,weiss,WSieg),!.
 
 %% Config-Block für schwarz %%
 
 config(wert_bauer,BWert):-
-	BWert is 1.
+	config(wert_bauer,schwarz,BWert).
 config(wert_offizier,OWert):-
-	OWert is 50.
+	config(wert_offizier,schwarz,OWert).
 config(wert_rand,RWert):-
-	RWert is 5.
+	config(wert_rand,schwarz,RWert).
 config(wert_doppelTurm,DTWert):-
-	DTWert is 10.
+	config(wert_doppelTurm,schwarz,DTWert).
 config(wert_zug,ZWert):-
-	ZWert is 0.
+	config(wert_zug,schwarz,ZWert).
 config(wert_befreibarerTürme,BTWert):-
-	BTWert is 10.
+	config(wert_befreibarerTürme,schwarz,BTWert).
 config(wert_sieg,WSieg):-
-	WSieg is 10000.
-
-config(max_tiefe_ki,6).
-config(max_tiefe_ab_ki,8).
+	config(wert_sieg,schwarz,WSieg).
 
 config(wert_SprungLänge_x,1,SL1):-
 	fehler(nein,schwarz),
