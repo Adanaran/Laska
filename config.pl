c9c247167665cd3cc2a9178e9033091ef4379a89
Dan Sörgel <dsoergel@stud.hs-bremen.de>
2015-01-29 01:07:46 +0100

    Config_Dan

--- config.pl
+++ config.pl
@@ -1,17 +1,17 @@
 config(wert_bauer,1).
 config(wert_offizier,3).
 config(wert_rand,1).
-config(wert_doppelTurm,1).
+config(wert_doppelTurm,2).
 config(wert_zug,1).
-config(wert_befreibarerTürme,1).
+config(wert_befreibarerTürme,3).
 config(wert_sieg,10000).
-config(max_tiefe_ki,6).
+config(max_tiefe_ki,8).
 
-config(wert_SprungLänge_x,1,2):-!.
-config(wert_SprungLänge_x,2,5):-!.
-config(wert_SprungLänge_x,3,10):-!.
-config(wert_SprungLänge_x,4,20):-!.
-config(wert_SprungLänge_x,_,40).
+config(wert_SprungLänge_x,1,5):-!.
+config(wert_SprungLänge_x,2,10):-!.
+config(wert_SprungLänge_x,3,30):-!.
+config(wert_SprungLänge_x,4,60):-!.
+config(wert_SprungLänge_x,_,100).
 
 :- dynamic ki/1.
 
